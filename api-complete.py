bc76d8ae36118d11fca378dbc14cbb0b4b12e380
Stefan van den Eertwegh <stefan@corebyte.nl>
2022-04-03 10:41:11 +0200

    update

--- api-complete.py
+++ api-complete.py
@@ -89,5 +89,28 @@ for device in retreive_devices.json():
     retreive_folders_by_device = s.get(f"{server_host}/folders/by_device/{device['id']}/", headers=headers)
 
     for folder in retreive_folders_by_device.json():
-        print(folder)
+        folderp = folder['name']
+        
+        call_path_url = f'{http}://{ip_address}/sdcard/cpt/app/data_api.php?url=/app/objects/{folder}/*'  # Ad_out'
+        req = sh.get(call_path_url, headers=headers3)
+        data = req.json()['response']['data'][0]
+
+        for da in data:
+            check = ""
+            print(f"Server: --> Retreive all folders by device {device['name']}")
+
+            retreive_folders_by_device = s.get(f"{server_host}/paths/read/{folder['id']}/{da['path']}/{da['type']}/", headers=headers)
+            
+            
+            if Path.objects.filter(folder=folder_obj, name=da['path'], type_path=da['type']).count() == 0:
+                path_obj = Path.objects.create(folder=folder_obj, name=da['path'], type_path=da['type'])
+            else:
+                path_obj = Path.objects.filter(folder=folder_obj, name=da['path'], type_path=da['type'])[0]
+
+            gen_uuid = uuid.uuid4()
+            for child in da['slots']:
+                if child['name'] == 'out':
+                    Slot.objects.create(uuid=gen_uuid, path=path_obj, name=child['name'], slotType=child['slotType'], type=child['type'], value=child['value'])
+
+        
 
